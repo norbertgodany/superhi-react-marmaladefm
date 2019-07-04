57af639d21b139ef63b1855bff60f19f7460a12d
Norbert Godany <norbert.godany@gmail.com>
2019-07-04 21:53:48 +0200

    routeParams in app.js, added show component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
 /* global Mixcloud */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import FeaturedMix from './FeaturedMix'
-import Header from './Header'
-import Home from './Home'
-import Archive from './Archive'
-import About from './About'
+import FeaturedMix from "./FeaturedMix";
+import Header from "./Header";
+import Home from "./Home";
+import Archive from "./Archive";
+import About from "./About";
+import Show from "./Show";
 
-import mixesData from '../data/mixes'
+import mixesData from "../data/mixes";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       // whether a mix is currently playing
       playing: false,
       // the id of the current mix
-      currentMix: '',
+      currentMix: "",
       mixIds: mixesData,
       mixes: [],
       mix: null
-    }
+    };
   }
 
   // /djcity/dj-dime/
@@ -40,17 +40,17 @@ class App extends Component {
           `https://api.mixcloud.com${id}`
         );
         // always remember await whe using fetch in an async function
-        const data = await response.json()
+        const data = await response.json();
         // put the mix into our state
         this.setState((prevState, props) => ({
           // here we add our data onto the end of all of our previous state using the spread operator
           mixes: [...prevState.mixes, data]
         }));
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    })
-  }
+    });
+  };
 
   mountAudio = async () => {
     this.widget = Mixcloud.PlayerWidget(this.player);
@@ -72,7 +72,7 @@ class App extends Component {
     );
 
     console.log(this.widget);
-  }
+  };
 
   componentDidMount() {
     // when our app component mounted on the page our componentDidMount gets called, and we run our mountAudio widget
@@ -89,7 +89,7 @@ class App extends Component {
     playMix: mixName => {
       // if th mixname is the same as the currently playing mix
       // we want to pause it instead
-      const { currentMix } = this.state
+      const { currentMix } = this.state;
       if (mixName === currentMix) {
         // when our code sees the return statement it will stop running and exit
         return this.widget.togglePlay();
@@ -102,10 +102,9 @@ class App extends Component {
       // load a mix by it's name and start playing it immediately
       this.widget.load(mixName, true);
     }
-  }
+  };
 
   render() {
-
     // this makes a variable from the first mix in the array
     // if the array is empty, we assign it a default value of an empty object {}
     const [firstMix = {}] = this.state.mixes;
@@ -114,25 +113,44 @@ class App extends Component {
       <Router>
         <div>
           <div className="flex-l justify-end ">
-            <FeaturedMix {...this.state} {...this.actions} {...firstMix} id={firstMix.key} />
+            <FeaturedMix
+              {...this.state}
+              {...this.actions}
+              {...firstMix}
+              id={firstMix.key}
+            />
             <div className="w-50-l relative z-1">
               <Header />
               {/* Here we are passing down all the state and the actions object to the Home component */}
-              <Route exact path="/" render={() => <Home {...this.state} {...this.actions} />} />
-              <Route path="/archive" render={() => <Archive {...this.state} {...this.actions} />} />
+              <Route
+                exact
+                path="/"
+                render={() => <Home {...this.state} {...this.actions} />}
+              />
+              <Route
+                path="/archive"
+                render={() => <Archive {...this.state} {...this.actions} />}
+              />
               <Route path="/about" render={() => <About {...this.state} />} />
+              <Route
+                path="/show/:id"
+                // Here we pass in the route params so that we can access the url of the current show page
+                render={routeParams => (
+                  <Show {...routeParams} {...this.state} />
+                )}
+              />
             </div>
           </div>
           {/* AudioPlayer */}
           <iframe
             // this gets the element inside react
-            ref={player => this.player = player}
+            ref={player => (this.player = player)}
             className="db fixed bottom-0 z-5"
             width="100%"
             height="60"
             src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
-            frameBorder="0" >
-          </iframe>
+            frameBorder="0"
+          />
         </div>
       </Router>
     );
