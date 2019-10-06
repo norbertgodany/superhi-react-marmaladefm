7a65fc281ae59205420d3c7de8cab93a7440a7e4
NORBERT GODANY <norbert.godany@gmail.com>
2019-10-06 13:09:50 +0200

    player.js update

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,39 +1,41 @@
-/* global Mixcloud */
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import actions from "../store/actions";
+/*global Mixcloud*/
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import actions from '../store/actions';
 
 class Player extends Component {
+  // every time the props change we can get access to them here
   componentWillReceiveProps(nextProps) {
-
-    // when our widget is not ready, we return and ignore all the actions below
+    // when our widget is not ready, we return
+    // and ignore all the actions below
     if (!nextProps.widgetReady) {
       return;
     }
 
-    // if there is a new mix in the props
     if (nextProps.currentMix !== this.props.currentMix) {
+      // if there is a new mix in the props
       // start playing the mix
       this.widget.load(nextProps.currentMix, true);
-      //if the event is not coming from mixcloud, we want to toggle the play pause on our audio
+      // if the event hasn’t come from mixcloud, we want to
+      // toggle the play pause on our audio
     } else if (!nextProps.fromMixcloud) {
-      this.widget.togglePlay()
+      this.widget.togglePlay();
     }
   }
 
   mountAudio = async () => {
-    const { playMix, setWidgetReady } = this.props;
-
+    const {playMix, setWidgetReady} = this.props;
+    // when we use the this keyword, our widget is now accessible
+    // anywhere inside the component
     this.widget = Mixcloud.PlayerWidget(this.player);
-
-    // here we wait for the widget to be ready before continuing
+    // here we wait for our widget to be ready before continuing
     await this.widget.ready;
 
-    // here we set our widget state to be ready in redux so we can block anything
-    // from happening before its ready
+    // here we set our widget state to be ready in redux so
+    // we can block anything from happening before it's ready
     setWidgetReady(true);
 
-    // using the mixcloud widget we can detect when our audio is paused
+    // using the mixcloud widget events we can detect when our
     // audio has been paused, set playing state to false
     this.widget.events.pause.on(() =>
       playMix({
@@ -41,62 +43,62 @@ class Player extends Component {
         fromMixcloud: true
       })
     );
-    // audio is playing, set playing state to true
+    // audio is playing again, set playing state to true
     this.widget.events.play.on(() =>
       playMix({
         playing: true,
         fromMixcloud: true
       })
     );
-
-    console.log(this.widget);
   };
 
   componentDidMount() {
-    // when our app component mounted on the page our componentDidMount gets called, and we run our mountAudio widget
+    // when our app component is all loaded onto the page
+    // our componentDidMount gets called and we can be sure
+    // everything is ready, so we then run our mountAudio()
+    // method
     this.mountAudio();
   }
 
-  // we group together our methods into an action object
   actions = {
+    // we group our methods together inside of an object
+    // called actions
     togglePlay: () => {
       // we want to togglePlay() on our widget
       this.widget.togglePlay();
     },
     playMix: mixName => {
-      // if th mixname is the same as the currently playing mix
-      // we want to pause it instead
-      const { currentMix } = this.state;
+      // if the mixName is the same as the currently
+      // playing mix, we want to pause it instead
+      const {currentMix} = this.state;
       if (mixName === currentMix) {
-        // when our code sees the return statement it will stop running and exit
+        // when our code sees a return statement it will
+        // stop running here and exit
         return this.widget.togglePlay();
       }
       // update the currentMix in our state
-      // with the mixName
+      // with the the mixName
       this.setState({
         currentMix: mixName
       });
-      // load a mix by it's name and start playing it immediately
+      // load a new mix by its name and then
+      // start playing it immediately
       this.widget.load(mixName, true);
     }
   };
-
   render() {
     return (
       <iframe
-        // this gets the element inside react
-        ref={player => (this.player = player)}
-        className="db fixed bottom-0 z-5"
         width="100%"
         height="60"
         src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
         frameBorder="0"
+        className="db fixed bottom-0 z-5"
+        // this allows us to get the actual html element inside react
+        ref={player => (this.player = player)}
       />
     );
   }
 }
 
-export default connect(
-  state => state,
-  actions
-)(Player);
+export default connect(state => state, actions)(Player);
